bca8491071f76e95c570e71429a3e05aca1b85ff
me <me@gmail.com>
2023-10-10 09:53:20 +0200

    second commit: route parameter

--- server.js
+++ server.js
@@ -4,11 +4,32 @@ const app = express();
 
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-    res.send({
+const friends = [
+    {
+        id: 0,
+        name: "Albert Einstein"
+    },
+    {
         id: 1,
         name: "Sir Isaac Newton"
-    })
+    }
+];
+
+app.get('/friends', (req, res) => {
+    res.json(friends);
+});
+
+// /friends/22
+app.get('/friends/:friendId', (req, res) => {
+    const friendId = Number(req.params.friendId);
+    const friend = friends[friendId];
+    if (friend) {
+        res.status(200).json(friend);
+    } else {
+        res.status(404).json({
+            error: "Friend does not exist"
+        });
+    }
 });
 
 app.get('/messages', (req, res) => {

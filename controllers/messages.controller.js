9dd08ff9aa7af997bf90ea82d170e949338cf800
me <me@gmail.com>
2023-10-11 16:23:11 +0200

    eleventh commit: Sending Files

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,6 +1,11 @@
+// use path module to handle different ways OSs manage path (Es Mac use /; Windows use \)
+const path = require('path');
+
 // Use named function instead of arrow function to best debug management
 function getMessages(req, res) {
-    res.send('<ul><li>Helloo Albert!</li></ul>');
+    //res.send('<ul><li>Helloo Albert!</li></ul>');
+    const myFile = path.join(__dirname,'..', 'public', 'skimountain.jpg')//__dirname avoid absolute path declaration but remeber to specify the position with '..'
+    res.sendFile(myFile)
 };
 
 function postMessage(req, res) {

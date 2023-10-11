e28657fc26e917010d0706ed3bb5e97d3b31a0cc
me <me@gmail.com>
2023-10-11 09:39:58 +0200

    seventh commit: MVC set controllers

--- controllers/friends.controller.js
+++ controllers/friends.controller.js
@@ -1,4 +1,23 @@
+// Use named function instead of arrow function to best debug management
 function postFriend(req, res) {
+    if(!req.body.name) {
+        return res.status(400).json({
+            error: 'Missing friend name'
+        });
+    }
+    const newFriend = {
+        name: req.body.name, //We need middleware esxpress.json() to acces name property of the body
+        id: friends.length
+    }
+    friends.push(newFriend);
+    res.json(newFriend);
+};
+
+function getFriends(req, res) {
+    res.json(friends);
+};
+
+function getFriend(req, res) {
     const friendId = Number(req.params.friendId);
     const friend = friends[friendId];
     if (friend) {
@@ -8,4 +27,11 @@ function postFriend(req, res) {
             error: "Friend does not exist"
         });
     }
+};
+
+// Export functions
+module.exports = {
+    postFriend,
+    getFriends,
+    getFriend
 }
--- server.js
+++ server.js
@@ -31,39 +31,11 @@ app.use((req, res, next) => {
 app.use(express.json()); //Built-in Express Middleware function. this affect the next function
 
 // Route Handlers
-app.post('/friends', (req, res) => {
-    if(!req.body.name) {
-        return res.status(400).json({
-            error: 'Missing friend name'
-        });
-    }
-    const newFriend = {
-        name: req.body.name, //We need middleware esxpress.json() to acces name property of the body
-        id: friends.length
-    }
-    friends.push(newFriend);
-    res.json(newFriend);
-});
-
-app.get('/friends', (req, res) => {
-    res.json(friends);
-});
-
-// /friends/22
-app.get('/friends/:friendId', (req, res) => {
-    const friendId = Number(req.params.friendId);
-    const friend = friends[friendId];
-    if (friend) {
-        res.status(200).json(friend);
-    } else {
-        res.status(404).json({
-            error: "Friend does not exist"
-        });
-    }
-});
+app.post('/friends', friendController.postFriend);
+app.get('/friends', friendController.getFriends);
+app.get('/friends/:friendId', friendController.getFriend); // /friends/22
 
 app.get('/messages', messageController.getMessages);
-
 app.post('/messages', messageController.postMessage);
 
 app.listen(PORT, ()=>{
